2db14c543bebf8c5095b8ccc61d809c3d0bd60fd
Iury Roque <iury.roque@hotmail.com>
2022-09-22 17:45:27 -0300

    Create a navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Products from './pages/products';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/products" element={ <Products /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Home /> } />
       </Routes>
