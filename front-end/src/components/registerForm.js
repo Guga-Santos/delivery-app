fea547a49500991aec729508ed34d07ce648d17d
alansouza1 <alansouza4001@gmail.com>
2022-09-22 15:17:27 -0300

    feat: elementos no form de register

--- front-end/src/components/registerForm.js
+++ front-end/src/components/registerForm.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import '../App.css';
 
-export default function LoginForm() {
+export default function RegisterForm() {
   return (
     <div className="register-container">
-      {}
+      <label htmlFor="name-input">
+        <input
+          type="text"
+          name="name-input"
+          id="name-input"
+          data-testid="common_register__input-name"
+        />
+      </label>
+      <label htmlFor="email-input">
+        <input
+          type="email"
+          name="email-input"
+          id="email-input"
+          data-testid="common_register__input-email"
+        />
+      </label>
+      <label htmlFor="password-input">
+        <input
+          type="password"
+          name="password-input"
+          id="password-input"
+          data-testid="common_register__input-password"
+        />
+      </label>
+      <button
+        className="register-buttons"
+        type="button"
+        data-testid="common_register__button-register"
+      >
+        CADASTRAR
+      </button>
     </div>
   );
 }
