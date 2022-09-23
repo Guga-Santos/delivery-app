b3fdebf2365a6bb3e090ec9805a4182626251f8e
Iury Roque <iury.roque@hotmail.com>
2022-09-23 15:20:07 -0300

    Feat: Add route products

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductCard from './components/productCard';
 import Home from './pages/home';
 import Login from './pages/login';
 import Products from './pages/products';
@@ -8,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/products" element={ <Products /> } />
+        <Route path="customer/products" element={ <Products /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Home /> } />
       </Routes>
