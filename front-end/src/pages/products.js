fa8ecac936a17cc7ab05a3ce2b00ea11d912dd5c
GugaSantos <guga.santos.guga@gmail.com>
2022-09-23 23:54:37 -0300

    feat: navigate to checkout route

--- front-end/src/pages/products.js
+++ front-end/src/pages/products.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/navBar';
 import ProductCard from '../components/productCard';
 import NewCartContext from '../context/newCartContext';
@@ -23,31 +24,45 @@ export default function Products() {
     findAllProducts();
   }, [setCart]);
 
+  const navigate = useNavigate();
+
   return (
     <div>
       <NavBar>produtos</NavBar>
       <main
-        style={ {
-          display: 'flex',
-          flexWrap: 'wrap' } }
+        style={ { display: 'flex', flexDirection: 'column' } }
       >
-        <p data-testid="customer_products__checkout-bottom-value">
-          {cart
-            .map((obj) => obj.price)
-            .reduce((a, b) => a + b, 0)
-            .toFixed(2).replace(/\./, ',')}
-        </p>
-        {
-          products?.map((product, index) => (
-            <ProductCard
-              key={ product.id }
-              id={ index + 1 }
-              name={ product.name }
-              price={ product.price }
-              urlImage={ product.url_image }
-            />
-          ))
-        }
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+          disabled={ cart.length < 1 }
+          onClick={ () => navigate('/customer/checkout') }
+          style={ { height: '4vh', width: '6vw' } }
+        >
+          <p data-testid="customer_products__checkout-bottom-value">
+            {cart
+              .map((obj) => obj.price)
+              .reduce((a, b) => a + b, 0)
+              .toFixed(2).replace(/\./, ',')}
+          </p>
+        </button>
+        <div
+          style={ {
+            display: 'flex',
+            flexWrap: 'wrap' } }
+        >
+          {
+            products?.map((product, index) => (
+              <ProductCard
+                key={ product.id }
+                id={ index + 1 }
+                name={ product.name }
+                price={ product.price }
+                urlImage={ product.url_image }
+              />
+            ))
+          }
+        </div>
       </main>
     </div>
   );
