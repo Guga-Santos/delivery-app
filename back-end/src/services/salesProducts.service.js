86c016a74cba2e738776874f6600b42d45e075ec
GugaSantos <guga.santos.guga@gmail.com>
2022-09-25 00:01:01 -0300

    stashed

--- back-end/src/services/salesProducts.service.js
+++ back-end/src/services/salesProducts.service.js
@@ -1,7 +1,18 @@
 const Model = require('../database/models');
 
 const SalesProductsService = {
+  create: async (data) => {
+    const sale = await Model.salesProducts.create(data);
+
+    return sale;
+  }, 
   getAll: async () => {
+    const sales = await Model.salesProducts.findAll();
     
-  }
-}
+    if (sales.length < 1) return null;
+
+    return sales;
+  },
+};
+
+module.exports = SalesProductsService;
